c71ffafca3717905964a4429514d40a1e9ae29ab
darshankerkar <darshankerkar09@gmail.com>
2026-04-08 22:21:42 +0530

    Clean up repo - keep only frontend and backend folders

--- _school_project_root/pagination.py
+++
@@ -1,8 +0,0 @@
-# Custom Pagination (create in root and use anywhere by importing)
-
-from rest_framework.pagination import PageNumberPagination
-
-class MyPagination(PageNumberPagination):
-    page_size=3
-    page_size_query='size'
-    max_page_size=10
--- students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('studentlist/', views.student_details, name='student_details'),
-    path('studentlist/<int:id>/', views.student_detail_view),
-]
